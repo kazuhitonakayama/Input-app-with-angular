5b8f4ae2006880c258755158903f88a7170240b8
kazuhito_nakayama <knakayama@valt.jp>
2019-11-06 16:56:24 +0900

    Dotinstall Angular.js入門

--- myscript.js
+++ myscript.js
@@ -3,12 +3,19 @@ angular.module('myapp', [])
         $scope.users = [
             {"name":"taguchi", "score":52.22},
             {"name":"tanaka", "score":38.22},
-            {"name":"yamada", "score":11.11}
+            {"name":"sakai", "score":52.22},
+            {"name":"akasi", "score":38.22},
+            {"name":"takeuchi", "score":52.22},
+            {"name":"takeda", "score":38.22},
+            {"name":"nakayama", "score":52.22},
+            {"name":"takeda", "score":38.22},
+            {"name":"yamadani", "score":11.11}
         ];
+        $scope.addUser = function() {
+        };
     }])
     .controller('UserItemController', ['$scope', function($scope) {
         $scope.increment = function() {
             $scope.user.score++;
         };
     }]);
-
